58bd42e7604956dc8789126fe6d0d8b601ce1db5
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-09-16 23:00:28 +0100

    JS challenge complete

---
+++ javascript-challenges/Build JavaScript Objects.js
@@ -0,0 +1,20 @@
+// Example
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+    "name": "Max",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Me!"]
+
+
+
+
+};
