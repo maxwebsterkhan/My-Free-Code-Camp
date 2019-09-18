f3b27c0df560d03750d677374c773b75f5d1eb0b
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-09-18 19:14:22 +0100

    js challenge complete

---
+++ javascript-challenges/Testing Objects for Properties.js
@@ -0,0 +1,18 @@
+// Setup
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+    if (myObj.hasOwnProperty(checkProp) == true) {
+        return myObj[checkProp];
+    }
+    else {
+        return "Not Found"
+        return "Change Me!";
+    }
+}
+// Test your code by modifying these values
+checkObj("gift");
