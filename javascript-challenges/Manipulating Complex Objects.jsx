369290688d5c404b5d1e86ed1bf465d06bd69d6d
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-09-19 14:10:00 +0100

    js challenge complete

---
+++ javascript-challenges/Manipulating Complex Objects.jsx
@@ -0,0 +1,24 @@
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    // Add record here
+    {
+
+        "artist": "Eminem",
+        "title": "Killshot",
+        "release_year": 2018,
+        "formats": [
+            "CD",
+            "Mp3",
+        ],
+    }
+];
