e16e4e9710630a2db947d83093668aae405d5ce0
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-09-21 22:54:35 +0100

    reformated code

--- javascript-challenges/Comparison with the Less Than Or Equal To Operator.js
+++ javascript-challenges/Comparison with the Less Than Or Equal To Operator.js
@@ -1,9 +1,9 @@
 function testLessOrEqual(val) {
-  if (val<=12) {  // Change this line
+  if (val <= 12) {  // Change this line
     return "Smaller Than or Equal to 12";
   }
-  
-  if (val<=24) {  // Change this line
+
+  if (val <= 24) {  // Change this line
     return "Smaller Than or Equal to 24";
   }
 
