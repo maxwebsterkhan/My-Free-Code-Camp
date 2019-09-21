94b07046129167a1331624d5166c3eaa29679450
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-09-21 22:55:37 +0100

    js challenge complete

--- javascript-challenges/Modify Array Data With Indexes.js
+++ javascript-challenges/Modify Array Data With Indexes.js
@@ -1,11 +1,11 @@
 // Example
-var ourArray = [18,64,99];
+var ourArray = [18, 64, 99];
 console.log("before change " + ourArray);
 ourArray[1] = 45; // ourArray now equals [18,45,99].
 console.log("after change " + ourArray);
 // Setup
 
-var myArray = [18,64,99];
+var myArray = [18, 64, 99];
 console.log("before change " + myArray);
 // Only change code below this line.
 myArray[0] = 45;
