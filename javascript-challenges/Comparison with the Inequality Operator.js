04be8f9ef5d7547fbcd04b5edc16c28d5a349e20
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-09-23 21:46:58 +0100

    reformat

--- javascript-challenges/Comparison with the Inequality Operator.js
+++ javascript-challenges/Comparison with the Inequality Operator.js
@@ -1,13 +1,13 @@
 // Setup
 function testNotEqual(val) {
-  if (val !=99) { // Change this line
+  if (val != 99) { // Change this line
     return "Not Equal";
   }
   return "Equal";
 }
 
 // Change this value to test
-var notEqual= testNotEqual(10);
-var equal= testNotEqual(99);
-console.log("10 is "+ notEqual + " to 99"); 
-console.log("99 is "+ equal + " to 99"); 
+var notEqual = testNotEqual(10);
+var equal = testNotEqual(99);
+console.log("10 is " + notEqual + " to 99");
+console.log("99 is " + equal + " to 99"); 
