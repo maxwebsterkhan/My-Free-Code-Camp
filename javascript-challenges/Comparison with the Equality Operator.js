4f3223c02be332640af2c1ede13df838409c9e38
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-09-23 21:47:28 +0100

    reformated

--- javascript-challenges/Comparison with the Equality Operator.js
+++ javascript-challenges/Comparison with the Equality Operator.js
@@ -1,13 +1,13 @@
 // Setup
 function testEqual(val) {
-  if (val ==12) { // Change this line
+  if (val == 12) { // Change this line
     return "Equal";
   }
   return "Not Equal";
 }
 
 // Change this value to test
-var tenNotEqual=testEqual(10);
-var twelveEqual=testEqual(12);
-console.log("10 is "+ tenNotEqual + " to 12"); 
-console.log("12 is "+ twelveEqual + " to 12"); 
+var tenNotEqual = testEqual(10);
+var twelveEqual = testEqual(12);
+console.log("10 is " + tenNotEqual + " to 12");
+console.log("12 is " + twelveEqual + " to 12"); 
