71eb161bf00a7685ab4d90869bfc94337ec4c488
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-09-23 21:46:24 +0100

    reformat

--- javascript-challenges/Comparison with the Strict Inequality Operator.js
+++ javascript-challenges/Comparison with the Strict Inequality Operator.js
@@ -1,10 +1,10 @@
 // Setup
 function testStrictNotEqual(val) {
   // Only Change Code Below this Line
-  
-  if (val!==17) {
 
-  // Only Change Code Above this Line
+  if (val !== 17) {
+
+    // Only Change Code Above this Line
 
     return "Not Equal";
   }
