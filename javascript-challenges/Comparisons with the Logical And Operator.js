371d3d975570219ffe921a35fa1c1d776709f33d
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-09-23 21:46:12 +0100

    reformat

--- javascript-challenges/Comparisons with the Logical And Operator.js
+++ javascript-challenges/Comparisons with the Logical And Operator.js
@@ -2,11 +2,11 @@ function testLogicalAnd(val) {
   // Only change code below this line
 
   if (val <= 50 && val >= 25) {
-      return "Yes";
-    }
+    return "Yes";
+  }
 
   // Only change code above this line
   return "No";
-   }
+}
 // Change this value to test
 testLogicalAnd(25);
