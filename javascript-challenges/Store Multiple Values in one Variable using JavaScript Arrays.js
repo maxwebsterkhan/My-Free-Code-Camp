6b7bc987d2822367e9968be46f077bbc37d7f612
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-10-04 23:09:48 +0100

    console log testing

--- javascript-challenges/Store Multiple Values in one Variable using JavaScript Arrays.js
+++ javascript-challenges/Store Multiple Values in one Variable using JavaScript Arrays.js
@@ -3,3 +3,5 @@ var ourArray = ["John", 23];
 
 // Only change code below this line.
 var myArray = ["Max", 87];
+console.log(myArray)
+console.log(ourArray)
--- javascript-challenges/Understand String Immutability.js
+++ javascript-challenges/Understand String Immutability.js
@@ -4,4 +4,4 @@ var myStr = "Jello World";
 // Only change code below this line
 
 myStr = "Hello World"; // Fix Me
-
+console.log(myStr)
