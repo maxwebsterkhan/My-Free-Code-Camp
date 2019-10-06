82d36fa0444b25ac407b9a4db099b8615eb42aaa
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-10-06 20:06:06 +0100

    rewrote and solved challenge again

--- javascript-challenges/Record Collection.js
+++ javascript-challenges/Record Collection.js
@@ -1,48 +1,48 @@
 // Setup
 var collection = {
-    "2548": {
-      "album": "Slippery When Wet",
-      "artist": "Bon Jovi",
-      "tracks": [ 
-        "Let It Rock", 
-        "You Give Love a Bad Name" 
-      ]
-    },
-    "2468": {
-      "album": "1999",
-      "artist": "Prince",
-      "tracks": [ 
-        "1999", 
-        "Little Red Corvette" 
-      ]
-    },
-    "1245": {
-      "artist": "Robert Palmer",
-      "tracks": [ ]
-    },
-    "5439": {
-      "album": "ABBA Gold"
-    }
+  "2548": {
+    "album": "Slippery When Wet",
+    "artist": "Bon Jovi",
+    "tracks": [
+      "Let It Rock",
+      "You Give Love a Bad Name"
+    ]
+  },
+  "2468": {
+    "album": "1999",
+    "artist": "Prince",
+    "tracks": [
+      "1999",
+      "Little Red Corvette"
+    ]
+  },
+  "1245": {
+    "artist": "Robert Palmer",
+    "tracks": []
+  },
+  "5439": {
+    "album": "ABBA Gold"
+  }
 };
 // Keep a copy of the collection for tests
 var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 // Only change code below this line
 function updateRecords(id, prop, value) {
-  var record = collection[id] 
-  if (prop === "tracks" && value !=="") {
-       if (record[prop]) {
-    record[prop].push(value);
-   }
-   else {
-    record[prop]=[value];
-   }
-   } else if (value !== "") {
-    record[prop] = value;
+  if (value == "") {
+    delete collection[id][prop];
+  } else {
+    if (prop != "tracks" && value != "") {
+      collection[id][prop] = value;
+    }
+    if (prop == "tracks" && !collection[id].hasOwnProperty("tracks")) {
+      collection[id][prop] = []
+    }
+    if (prop == "tracks" && value != "") {
+      collection[id][prop].push(value);
+    }
   }
-  else {
-    delete record[prop];
-  }
+
   return collection;
 }
 
