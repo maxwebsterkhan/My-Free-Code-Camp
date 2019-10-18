015541f788dbcfc7a8ad9c29055e60accc4bfa23
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-10-18 19:15:25 +0100

    removed white space

--- javascript-challenges/Stand in Line.js
+++ javascript-challenges/Stand in Line.js
@@ -4,10 +4,8 @@ function nextInLine(arr, item) {
   var removed = arr.shift();
   return removed;  // Change this line
 }
-
 // Test Setup
 var testArr = [1,2,3,4,5];
-
 // Display Code
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6)); // Modify this line to test
