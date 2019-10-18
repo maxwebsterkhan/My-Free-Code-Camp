c93e936590aa719f3594f71c3fb9fffcfd5a16e1
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-10-18 19:13:55 +0100

    removed whitespace

--- javascript-challenges/Use Bracket Notation to Find the Last Character in a String.js
+++ javascript-challenges/Use Bracket Notation to Find the Last Character in a String.js
@@ -1,10 +1,8 @@
 // Example
 var firstName = "Ada";
 var lastLetterOfFirstName = firstName[firstName.length - 1];
-
 // Setup
 var lastName = "Lovelace";
-
 // Only change code below this line.
 var lastLetterOfLastName = lastName[lastName.length - 1];
 
