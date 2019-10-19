f21247f13bd4d387c3dd2edf71a754f0f239714f
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-10-19 21:30:28 +0100

    removed whitespace

--- javascript-challenges/Passing Values to Functions with Arguments.js
+++ javascript-challenges/Passing Values to Functions with Arguments.js
@@ -3,9 +3,7 @@ function ourFunctionWithArgs(a, b) {
   console.log(a - b);
 }
 ourFunctionWithArgs(10, 5); // Outputs 5
-
 // Only change code below this line.
-
 function functionWithArgs(a, b) {
   console.log(a + b)
 }
