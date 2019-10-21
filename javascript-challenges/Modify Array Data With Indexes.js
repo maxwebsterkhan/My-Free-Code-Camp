ddf5fb334ed51d48456a202bbe6ecb445da4a160
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-10-21 19:23:11 +0100

    removed white space

--- javascript-challenges/Modify Array Data With Indexes.js
+++ javascript-challenges/Modify Array Data With Indexes.js
@@ -4,7 +4,6 @@ console.log("before change " + ourArray);
 ourArray[1] = 45; // ourArray now equals [18,45,99].
 console.log("after change " + ourArray);
 // Setup
-
 var myArray = [18, 64, 99];
 console.log("before change " + myArray);
 // Only change code below this line.
