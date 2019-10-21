7d346ae6e5cfaef5c27cadb4234462668bfed787
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-10-21 19:22:54 +0100

    removed white space

--- javascript-challenges/Multiple Identical Options in Switch Statements.js
+++ javascript-challenges/Multiple Identical Options in Switch Statements.js
@@ -7,13 +7,11 @@ function sequentialSizes(val) {
         case 3:
             return "Low";
             break;
-
         case 4:
         case 5:
         case 6:
             return "Mid";
             break;
-
         case 7:
         case 8:
         case 9:
@@ -21,11 +19,8 @@ function sequentialSizes(val) {
             break;
 
     }
-
-
     // Only change code above this line  
     return answer;
 }
-
 // Change this value to test
 console.log(sequentialSizes(1));
