2b20e4edf294f2b42f8b18e3c5246078b06a9fae
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-10-23 19:51:21 +0100

    removed white space

--- javascript-challenges/Iterate Through an Array with a For Loop.js
+++ javascript-challenges/Iterate Through an Array with a For Loop.js
@@ -1,11 +1,9 @@
 // Example
 var ourArr = [9, 10, 11, 12];
 var ourTotal = 0;
-
 for (var i = 0; i < ourArr.length; i++) {
     ourTotal += ourArr[i];
 }
-
 // Setup
 var myArr = [2, 3, 4, 5, 6];
 var total = 0;
