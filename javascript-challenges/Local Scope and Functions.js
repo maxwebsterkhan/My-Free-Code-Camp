49448613cbc369c91102c44c896023c55de47080
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-10-23 19:50:42 +0100

    removed white spce

--- javascript-challenges/Local Scope and Functions.js
+++ javascript-challenges/Local Scope and Functions.js
@@ -4,9 +4,6 @@ function myLocalScope() {
   console.log(myVar);
 }
 myLocalScope();
-
 // Run and check the console
 // myVar is not defined outside of myLocalScope
-
-
 // Now remove the console log line to pass the test
