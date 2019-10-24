cb73c4de32a614b98230bf24c5350fff58e54ac3
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-10-24 18:43:31 +0100

    removed whitespace

--- javascript-challenges/Global Scope and Functions.js
+++ javascript-challenges/Global Scope and Functions.js
@@ -1,11 +1,9 @@
 // Declare your variable here
-
 var myGlobal = 10;
 function fun1() {
   // Assign 5 to oopsGlobal Here
   oopsGlobal = 5;
 }
-
 function fun2() {
   var output = "";
   if (typeof myGlobal != "undefined") {
