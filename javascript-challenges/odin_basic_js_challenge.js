5376058513d26ccd3f33f2993506cb408c766d4e
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-11-04 10:21:17 +0000

    part  2 of challenge complete

---
+++ javascript-challenges/odin_basic_js_challenge.js
@@ -0,0 +1,14 @@
+console.log(23 + 97)
+let addition = 1 + 2 + 3 + 4 + 5 + 6;
+console.log(addition)
+let equation = (4 + 6 + 9) / 77;
+console.log(equation)
+let a = 10;
+console.log(a)
+9 * a
+let b = 7 * a;
+console.log(b)
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage)
