319c333d7b93416770335cad0a1ec6f1d98db4df
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-11-05 13:19:58 +0000

    fixed code order

--- javascript-challenges/odin_function_practice.js
+++ javascript-challenges/odin_function_practice.js
@@ -1,8 +1,10 @@
 function add7(number) {
- console.log(number + 7)
+ return (number + 7)
 }
-add7(7)
+console.log(add7(7))
 
 function multiply(n1, n2){
-console.log(n1 * n2)}
-multiply(1, 2)
+return(n1 * n2)}
+console.log(multiply(1, 2))
+
+
