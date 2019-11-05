963fb8e62b5a31656cf2dff94ee294a4e5ddc401
Max Webster-Khan <maxwebsterr@live.co.uk>
2019-11-05 13:37:11 +0000

    finished all challenges and removed whitespace

--- javascript-challenges/odin_function_practice.js
+++ javascript-challenges/odin_function_practice.js
@@ -1,10 +1,16 @@
 function add7(number) {
- return (number + 7)
+    return (number + 7)
 }
 console.log(add7(7))
-
-function multiply(n1, n2){
-return(n1 * n2)}
+function multiply(n1, n2) {
+    return (n1 * n2)
+}
 console.log(multiply(1, 2))
-
-
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+console.log(capitalize("hello my good sir"))
+function lastletter(str) {
+    return str[str.length - 1];;
+}
+console.log(lastletter("abcde"));
